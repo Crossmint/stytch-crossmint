9615a1cdd530b5b1a37ab96951b1bf67bedbf3df
PaellaRosh <113327185+PaellaRosh@users.noreply.github.com>
2023-03-03 16:32:24 -0500

    Fix build

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
+import { StytchProvider } from "@stytch/nextjs";
+import { createStytchUIClient } from "@stytch/nextjs/ui";
+
+const stytch = createStytchUIClient(process.env.NEXT_PUBLIC_STYTCH_PUBLIC_TOKEN);
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <StytchProvider stytch={stytch}>
+        <Component {...pageProps} />
+      </StytchProvider>
+  );
 }
